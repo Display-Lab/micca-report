5edd3b81dccc29f233cd62440624b8672224b98e
Colin Gross <grosscol@gmail.com>
2020-05-06 15:25:28 -0400

    Lower case all chr cols for consistency.

--- hurley_ingest.R
+++ hurley_ingest.R
@@ -23,22 +23,26 @@ read_hurley_data <- function(filename){
 
 normalize_hurley_data <- function(raw_data){
   
-  # ASSUME: D7 larc_prov is "4-60 at PP visit" for "4-60 days"
+  # ASSUME: D7 larc_prov is "4-60 at pp visit" for "4-60 days"
+  # Enforce choice: immediate PP IUD, immediate PP Nexplanon, PPTL, other, none, unknown
+    # 
   # Clean input data.
   #  Currently unused: choice_date = ymd(base::strptime(choice_date, format="%Y-%m-%d")),
   raw_data %>% 
     filter(!is.na(mrn))  %>%
     mutate(
       counseling      = ifelse(counseling == 'Yes', TRUE, FALSE),
-      imm_method      = recode(.x=imm_method, "none during delivery admission"="None"),
+      imm_method      = recode(.x=imm_method, "none during delivery admission"="none"),
       contra_prov     = sub("(\\d)days","\\1 days",contra_prov),
       larc_prov       = sub("(\\d)days","\\1 days",larc_prov),
-      larc_prov       = ifelse(larc_prov == "4-60 days", "4-60 at PP visit", larc_prov),
+      larc_prov       = ifelse(larc_prov == "4-60 days", "4-60 days pp visit", larc_prov),
       payer           = sub(pattern = "-.*$", replacement = "", payer),
       institution = "Hurley" ) %>%
-    filter(payer %in% c("Private", "Medicaid", "Other"))
-  
+    mutate_if(.predicate=is.character, tolower) %>%
+    filter(payer %in% c("medicaid", "molina", "private", "other", "unknown"))
 }
+
+
 
 calculate_hurley_components <- function(proc_data){
   proc_data %>% 
@@ -46,22 +50,22 @@ calculate_hurley_components <- function(proc_data){
            quarter = as.yearqtr(delivery_date)) %>%
     group_by(report_month, payer) %>%
     summarize(
-      C1 = sum(!is.na(delivery_date)),
+      C1 = n(),
       C2 = sum(contra_prov == "0-3 days"),
       C3 = sum(contra_prov == "4-60 days"),
       C4 = sum(larc_prov == "0-3 days"),
-      C5 = sum(larc_prov == "4-60 days"),
-      C6 = length(unique(mrn)),
+      C5 = sum(larc_prov == "4-60 days pp visit" | larc_prov == "4-60 days not pp visit" ),
+      #C6 = NA, # deprecated
       C7 = sum(counseling),
-      C8 = sum(contra_choice != "None"),
-      C9 = sum(contra_choice == "immediate PP IUD" | contra_choice == "immediate PP Nexplanon"),
-      C10 = sum(!is.na(delivery_date) & contra_choice != "None"),
-      C11 = sum(!is.na(delivery_date) & counseling & contra_choice == imm_method),
-      C12 = sum(!is.na(delivery_date) & larc_prov == "4-60 days"),
-      C13 = sum(contra_choice == "immediate PP IUD"),
-      C14 = sum(contra_choice == "immediate PP Nexplanon"),
-      C15 = sum(contra_choice == "PPTL"),
-      C16 = sum(contra_choice == "Other")
+      C8 = sum(contra_choice != "none"),
+      C9 = sum(contra_choice == "immediate pp iud" | contra_choice == "immediate pp nexplanon"),
+      #C10 = sum(contra_choice != "none"), # deprecated
+      C11 = sum(counseling & contra_choice == imm_method),
+      C12 = sum(larc_prov == "4-60 days pp visit" | larc_prov == "4-60 days not pp visit"),
+      C13 = sum(contra_choice == "immediate pp iud"),
+      C14 = sum(contra_choice == "immediate pp nexplanon"),
+      C15 = sum(contra_choice == "pptl"),
+      C16 = sum(contra_choice == "other")
     )
 }
 
@@ -77,17 +81,16 @@ calculate_measures <- function(component_data){
       n_M2  = C3,  d_M2  = C1,
       n_M3  = C4,  d_M3  = C1,
       n_M4  = C5,  d_M4  = C1,
-      n_M5  = C7,  d_M5  = C6,
-      n_M6  = C8,  d_M6  = C6,
+      n_M5  = C7,  d_M5  = C1,
+      n_M6  = C8,  d_M6  = C1,
       n_M7  = C9,  d_M7  = C8,
-      n_M8  = C11, d_M8  = C10,
+      n_M8  = C11, d_M8  = C8,
       n_M9  = C12, d_M9  = C1,
       n_M10 = C13, d_M10 = C8,
       n_M11 = C14, d_M11 = C8,
       n_M12 = C15, d_M12 = C8,
       n_M13 = C16, d_M13 = C8,
-      n_M14 = C1,  d_M14 = NA,
-      n_M15 = C6,  d_M15 = NA) %>%
+      n_M14 = C1,  d_M14 = NA) %>%
       pivot_longer(cols = starts_with("n_"), names_to = "measure_n", 
                    names_prefix = "n_", values_to = "numerator") %>% 
       pivot_longer(cols = starts_with("d_"), names_to = "measure_d", 
@@ -112,7 +115,7 @@ mar_data <- normalize_hurley_data(mar_raw)
 
 # combine and emit normalized data for future reference by hurley
 norm_data <-  bind_rows(jan_data, feb_data, mar_data)
-write_csv(maptg, "hurley_2020_Q1.csv")
+write_csv(norm_data, "hurley_2020_Q1.csv")
 
 # Calc measures components by delivery month and payer
 jan_comp <- calculate_hurley_components(jan_data)

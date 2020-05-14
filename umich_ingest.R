249b624df29a1a1c6a50e3a09426e486dba8669d
Colin Gross <grosscol@gmail.com>
2020-05-14 11:52:11 -0400

    Ingest Umich data   Make normalized data from both sites copacetic.

--- umich_ingest.R
+++ umich_ingest.R
@@ -6,51 +6,38 @@ library(lubridate)
 library(stringr)
 library(zoo)
 
-read_hurley_data <- function(filename){
+read_umich_data <- function(filename){
+  
   # Ingest hurley data
   col_classes <- cols_only(col_character(), col_date(), col_character(),
                            col_character(),col_character(),col_character(),
-                           col_character(),col_character(),col_character(), 
+                           col_character(),col_character(),col_character(),
                            col_character())
   
-  # Force column names and hope order doesn't change.
-  #col_names <- c("mrn","delivery_date","counseling","contra_choice","imm_method",
-  #               "contra_prov","larc_prov","payer","choice_date")
-  
+  # This column order is different than the specification
   col_names <- c("mrn","delivery_date","counseling","contra_choice","imm_method",
-                 "contra_prov","larc_prov","payer","choice_date","peripartum_care")
+                 "contra_prov","larc_prov","choice_date","payer", "peripartum_care")
   
   raw_data <- readr::read_csv(filename, col_types = col_classes, na=c(""))
   colnames(raw_data) <- col_names
   return(raw_data)
 }
 
-normalize_hurley_data <- function(raw_data){
-  # ASSUME: D7 larc_prov is "4-60 at pp visit" for "4-60 days"
-  # Enforce choice: immediate PP IUD, immediate PP Nexplanon, PPTL, other, none, unknown
-  # Correct "Unkown" to "unknown"
-  # Recode choice to unknown for counseling == FALSE
-  #  Currently unused: choice_date = ymd(base::strptime(choice_date, format="%Y-%m-%d")),
+normalize_umich_data <- function(raw_data){
   raw_data %>% 
     filter(!is.na(mrn))  %>%
     mutate_if(.predicate=is.character, tolower) %>%
     mutate(
-      counseling      = ifelse(counseling == 'yes', TRUE, FALSE),
-      contra_choice   = ifelse(contra_choice == "unkown", "unknown", contra_choice),
-      contra_choice   = ifelse(counseling, contra_choice, "unknown"),
-      imm_method      = recode(.x=imm_method, "none during delivery admission"="none"),
-      contra_prov     = sub("(\\d)days","\\1 days",contra_prov),
-      larc_prov       = sub("(\\d)days","\\1 days",larc_prov),
-      larc_prov       = ifelse(larc_prov == "4-60 days", "4-60 days pp visit", larc_prov),
-      payer           = sub(pattern = "-.*$", replacement = "", payer),
-      peripartum_care = case_when( peripartum_care == "yes" ~ TRUE,
-                                   peripartum_care == "no" ~ FALSE),
-      institution = "Hurley" ) %>%
+      counseling      = ifelse(counseling == 'Yes', TRUE, FALSE),
+      payer           = sub(pattern = "molina", replacement = "medicaid", payer),
+      institution = "UMich" ) %>%
     filter(payer %in% c("medicaid", "molina", "private", "other", "unknown"),
-           contra_choice %in% c("immediate pp iud", "immediate pp nexplanon", "pptl", "other", "none", "unknown"))
+           contra_choice %in% c("immediate pp iud", "immediate pp nexplanon", "pptl", "other", "none", "unknown")) %>%
+    select(mrn,delivery_date,counseling, contra_choice, imm_method, contra_prov, 
+           larc_prov, payer, choice_date, peripartum_care)
 }
 
-calculate_hurley_components <- function(proc_data){
+calculate_components <- function(proc_data){
   proc_data %>% 
     mutate(report_month = floor_date(delivery_date, unit="month"),
            quarter = as.yearqtr(delivery_date)) %>%
@@ -81,9 +68,7 @@ calculate_hurley_components <- function(proc_data){
     )
 }
 
-# numerators and denominators by month and payer
 calculate_measures <- function(component_data){
-  # this currently drops quarter and choice month
   component_data %>%
     group_by(report_month, payer) %>%
     summarise_all(.funs=sum) %>%
@@ -118,41 +103,21 @@ calculate_measures <- function(component_data){
       select(-measure_d)
 }
 
-# Measure Numerators in terms of component
-
 # Read raw data
-jan_raw <- read_hurley_data('data/hurley_2020-01.csv')
-feb_raw <- read_hurley_data('data/hurley_2020-02.csv')
-mar_raw <- read_hurley_data('data/hurley_2020-03.csv')
+df_raw <- read_umich_data("data/umich_2020_Q1.csv")
+# Normalize
+df_norm <- normalize_umich_data(df_raw)
+# Emit normalized data
+write_csv(df_norm, "umich_norm_2015-07-01_2020-04-06.csv")
 
+# Calculate components and measures
+comp_df <- calculate_components(df_norm)
+measure_df <- calculate_measures(comp_df)
 
-# Normalize data
-jan_data <- normalize_hurley_data(jan_raw)
-feb_data <- normalize_hurley_data(feb_raw)
-mar_data <- normalize_hurley_data(mar_raw)
-
-# combine and emit normalized data for future reference by hurley
-norm_data <-  bind_rows(jan_data, feb_data, mar_data)
-write_csv(norm_data, "hurley_norm_2020_Q1.csv")
-
-# Calc measures components by delivery month and payer
-jan_comp <- calculate_hurley_components(jan_data)
-feb_comp <- calculate_hurley_components(feb_data)
-mar_comp <- calculate_hurley_components(mar_data)
-
-# Consolidate components from each month
-#  Problem binding yearqtr columns together.  Information is lost
-#  Related to: https://github.com/tidyverse/dplyr/issues/2457
-comp_data <- bind_rows(jan_comp, feb_comp, mar_comp)
-
-# calculate measures from components
-measure_data <- calculate_measures(comp_data)
-
-# make into maptg format
-maptg <- measure_data %>% 
+# Create MAPTG data
+maptg <- measure_df %>% 
   rename(time=report_month) %>%
-  add_column(ascribee="Hurley", .before=1)
+  add_column(ascribee="UMich", .before=1)
 
 # Emit cleaned data to file
-write_csv(maptg, "maptg_hurley_2020_Q1.csv")
-
+write_csv(maptg, "maptg_umich.csv")

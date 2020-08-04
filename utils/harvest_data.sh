1f294ded5ba32f2e851241240fded103fc132960
Colin Gross <grosscol@gmail.com>
2020-08-04 14:47:49 -0400

    Add get data from S3 script.

---
+++ utils/harvest_data.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DATA_BUCKET=${DATA_BUCKET:-data.larc.micca.report}
+
+# Create data directory for data
+mkdir -p site_data
+
+# Do dumb sync of all data
+aws s3 sync s3://${DATA_BUCKET}/data site_data/
+
+# Construct YMD_STRING
+# YMD_STRING='2020-03-01'
+# Query for the list of objects
+#
+# aws s3api list-objects --bucket "${DATA_BUCKET}"\
+#   --query 'Contents[?LastModified>='#{YMD_STRING}'][].{Key: Key}'
+# 
+# Pipe to jq to extract relevant info
+

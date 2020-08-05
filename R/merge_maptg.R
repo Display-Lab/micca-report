270f02063c71ef83e94ba6ab14d8f630d3e680fd
Colin Gross <grosscol@gmail.com>
2020-08-05 13:30:40 -0400

    Get pacakge building successfully

--- R/merge_maptg.R
+++ R/merge_maptg.R
@@ -1,20 +1,20 @@
-# Combine maptg data sets from multiple sites
-library(readr)
-library(dplyr)
+#' Combine maptg data sets from multiple sites
+#' @import readr dplyr
+merge_maptg_files <- function(){
+  maptg_colspec <- cols(ascribee = col_character(),
+                        time = col_date(format="%Y-%m-%d"),
+                        group = col_factor(),
+                        numerator = col_integer(),
+                        denominator = col_integer())
 
-maptg_colspec <- cols(ascribee = col_character(), 
-                      time = col_date(format="%Y-%m-%d"), 
-                      group = col_factor(),
-                      numerator = col_integer(),
-                      denominator = col_integer())
-  
-maptg_files <- c('data/maptg_hurley_2020_Q1.csv',
-                 'data/maptg_munson_2020_Q1.csv',
-                 'data/maptg_wmh_2020_Q1.csv',
-                 'data/maptg_umich.csv')
+  maptg_files <- c('proc-data/maptg_hurley_2020_Q1.csv',
+                   'proc-data/maptg_munson_2020_Q1.csv',
+                   'proc-data/maptg_wmh_2020_Q1.csv',
+                   'proc-data/maptg_umich.csv')
 
-df_list <- lapply(maptg_files, read_csv, col_types=maptg_colspec)
+  df_list <- lapply(maptg_files, read_csv, col_types=maptg_colspec)
 
-full_maptg_data <- Reduce(bind_rows, df_list)
+  full_maptg_data <- Reduce(bind_rows, df_list)
 
-write_csv(full_maptg_data, "data/maptg_consolidated.csv")
+  write_csv(full_maptg_data, "data/maptg_consolidated.csv")
+}
--- R/munson_ingest.R
+++ R/munson_ingest.R
@@ -1,34 +1,26 @@
-library(readr)
-library(dplyr)
-library(tidyr)
-library(tibble)
-library(lubridate)
-library(stringr)
-library(zoo)
-
-source('components_and_measures.R')
-
+#' @import readr
 read_munson_data <- function(filename){
   # Had to accomodate leading empty column
   # Date format in input data is bad
   col_classes <- cols_only(col_skip(),
                            col_character(), col_date(format="%m/%d/%Y"), col_character(),
                            col_character(), col_character(), col_character(),
-                           col_character(), col_character(), col_character(), 
+                           col_character(), col_character(), col_character(),
                            col_character())
-  
+
   # Force column names and hope order doesn't change.
   col_names <- c("mrn","delivery_date","counseling","contra_choice","imm_method",
                  "contra_prov","larc_prov","payer","choice_date","peripartum_care")
-  
+
   # Need to skip the first 19 rows where they've reproduced the example spreadsheet as reference.
   raw_data <- readr::read_csv(filename, col_types = col_classes, col_names=FALSE, na=c(""), skip=9)
   colnames(raw_data) <- col_names
   return(raw_data)
 }
 
+#' @import dplyr tidyr stringr
 standardize_munson_data <- function(raw_data){
-  raw_data %>% 
+  raw_data %>%
     filter(!is.na(mrn))  %>%
     mutate_if(.predicate=is.character, tolower) %>%
     mutate(
@@ -38,25 +30,28 @@ standardize_munson_data <- function(raw_data){
       institution = "Munson" ) %>%
     filter(payer %in% c("medicaid", "molina", "private", "other", "unknown"),
            contra_choice %in% c("immediate pp iud", "immediate pp nexplanon", "pptl", "other", "none", "unknown")) %>%
-    select(mrn,delivery_date,counseling, contra_choice, imm_method, contra_prov, 
+    select(mrn,delivery_date,counseling, contra_choice, imm_method, contra_prov,
            larc_prov, payer, choice_date, peripartum_care)
 }
 
-# Read raw data
-df_raw <- read_munson_data("data/munson_2020_Q1.csv")
+#' @import readr dplyr tidyr tibble lubridate stringr zoo
+ingest_munson <- function(){
+  # Read raw data
+  df_raw <- read_munson_data("data/munson_2020_Q1.csv")
 
-# Standardize
-df_std <- standardize_munson_data(df_raw)
+  # Standardize
+  df_std <- standardize_munson_data(df_raw)
 
-# Emit standardized data
-write_csv(df_std, "munson_std_2020_Q1.csv")
+  # Emit standardized data
+  write_csv(df_std, "munson_std_2020_Q1.csv")
 
-# Calculate components and measures
-df_comp <- calculate_components(df_std)
-df_measure <- calculate_measures(df_comp)
+  # Calculate components and measures
+  df_comp <- calculate_components(df_std)
+  df_measure <- calculate_measures(df_comp)
 
-# Create MAPTG data
-maptg <- make_maptg(df_measure, "Munson")
+  # Create MAPTG data
+  maptg <- make_maptg(df_measure, "Munson")
 
-# Emit cleaned data to file
-write_csv(maptg, "maptg_munson_2020_Q1.csv")
+  # Emit cleaned data to file
+  write_csv(maptg, "maptg_munson_2020_Q1.csv")
+}

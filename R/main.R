7eb7952dde2811032064e033931f12f7939076f2
Colin Gross <grosscol@gmail.com>
2020-08-12 10:58:11 -0400

    Correct debugging label generation. Prepare for v2 report swap in.

--- R/main.R
+++ R/main.R
@@ -13,7 +13,6 @@ main <- function(maptg_data, recip, output_path='report.pdf', include_cid=F){
   start_month <- format(obs_start_date, "%b")
   end_month <- format(obs_end_date, "%b")
   end_year <- format(obs_end_date, "%Y")
-  INCLUDE_CID=include_cid
   ascribee_title <- ifelse(is.na(MR$ASCRIBEE_TITLES[recip]), recip, MR$ASCRIBEE_TITLES[recip])
 
   #### SUMS: OVERVIEW
@@ -24,68 +23,68 @@ main <- function(maptg_data, recip, output_path='report.pdf', include_cid=F){
   #### FIGURE
   plot_data <- make_fig7F5D31_data(maptg_data, recip)
   fig7F5D31 <- make_fig7F5D31(plot_data)
-  content_id <- deparse(substitute(fig7F5D31))
+  content_id <- deparse(quote(fig7F5D31))
   info7F5D31 <- paste(content_id, "m14")
 
   #### FIGURE
   plot_mean <- micca_mean(maptg_data, "M1")
   figADA835A <- circle_plot(maptg_data, recip, "M1", plot_mean)
-  content_id <- deparse(substitute(fig7F5D31))
+  content_id <- deparse(quote(figADA835A))
   infoADA835A <- paste(content_id, "m1")
 
   #### FIGURE
   fig707A6E <- line_plot(maptg_data, recip, "M1")
-  content_id <- deparse(substitute(fig707A6E))
+  content_id <- deparse(quote(fig707A6E))
   info707A6E <- paste(content_id, "m1")
 
   #### FIGURE
   plot_mean <- micca_mean(maptg_data, "M3")
   fig9C0A4F <- circle_plot(maptg_data, recip, "M3", plot_mean)
-  content_id <- deparse(substitute(fig9C0A4F))
+  content_id <- deparse(quote(fig9C0A4F))
   info9C0A4F <- paste(content_id, "m3")
 
   ### FIGURE
   plot_data <- make_figBE214E_data(maptg_data, recip)
   figBE214E <- make_figBE214E(plot_data)
-  content_id <- deparse(substitute(figBE214E))
+  content_id <- deparse(quote(figBE214E))
   infoBE214E <- paste(content_id, "m20,21")
 
   #### FIGURE
   plot_mean <- micca_mean(maptg_data, "M5")
   fig540727 <- circle_plot(maptg_data, recip, "M5", plot_mean)
-  content_id <- deparse(substitute(fig540727))
+  content_id <- deparse(quote(fig540727))
   info540727 <- paste(content_id, "m5")
 
   #### FIGURE
   fig5BF5D0 <- line_plot(maptg_data, recip, "M5")
-  content_id <- deparse(substitute(fig5BF5D0))
+  content_id <- deparse(quote(fig5BF5D0))
   info5BF5D0 <- paste(content_id, "m5")
 
   #### TABLE DATA
   tbl82C4A3 <- make_table_data_tbl82C4A3(maptg_data, recip)
-  content_id <- deparse(substitute(tbl82C4A3))
+  content_id <- deparse(quote(tbl82C4A3))
   info82C4A3 <- paste(content_id, "m10,11,12,13")
 
   #### FIGURE
   plot_data <- make_fig1903AB_data(maptg_data, recip)
   fig1903AB <- make_fig1903AB(plot_data)
-  content_id <- deparse(substitute(fig1903AB))
+  content_id <- deparse(quote(fig1903AB))
   info1903AB <- paste(content_id, "m10,11,12,13")
 
   #### FIGURE
   plot_mean <- micca_mean(maptg_data, "M8")
   figBDBC81 <- circle_plot(maptg_data, recip, "M8", plot_mean)
-  content_id <- deparse(substitute(figBDBC81))
+  content_id <- deparse(quote(figBDBC81))
   infoBDBC81 <- paste(content_id, "m8")
 
   #### FIGURE
   plot_data <- make_figE8F578_data(maptg_data, recip)
   figE8F578 <- make_figE8F578(plot_data)
-  content_id <- deparse(substitute(figE8F578))
+  content_id <- deparse(quote(figE8F578))
   infoE8F578 <- paste(content_id, "m16,17,18,19")
 
   # Generate Report
-  template_path <- system.file("templates","report.Rnw", package="miccareport")
+  template_path <- system.file("templates","report_v1.Rnw", package="miccareport")
 
   build_dir <- tempdir()
 
@@ -101,17 +100,6 @@ main <- function(maptg_data, recip, output_path='report.pdf', include_cid=F){
 ######################
 # Process Input Data #
 ######################
-#' @note DEPRECATED
-trim_input <- function(){
-  full_maptg_data <- readr::read_csv('data/maptg.csv', trim_ws=T)
-
-  # Trim to date range
-  START_DATE <- lubridate::ymd('2020-01-01')
-  END_DATE <- lubridate::ymd('2020-04-01')
-  trimmed_data <- full_maptg_data %>% filter( time >= START_DATE, time < END_DATE)
-  return(trimmed_data)
-}
-
 #' @param data maptg dataframe
 #' @param begin yyyy-MM-dd format date of begin date inclusive
 #' @param end yyyy-MM-dd format date of end date excluded

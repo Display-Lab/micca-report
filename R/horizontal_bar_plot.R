2f3e5eeb0cebedfa3408775a2a457556ae2a67e4
Colin Gross <grosscol@gmail.com>
2020-08-31 18:46:13 -0400

    Fix Horizontal bar plot x-axis spacing.

--- R/horizontal_bar_plot.R
+++ R/horizontal_bar_plot.R
@@ -1,15 +1,18 @@
 #' Horizontal Bar Plot
-#' @param maptg_data long form tabular data in maptg format
+#' @param data long form tabular data in maptg format
 #' @param recip name of recipient as it appears in the data
 #' @param measures vector of measure ids from which to create bars.
-horizontal_bar_plot <- function(maptg_data, recip, measures){
-  plot_data <- hbar_plot_data(maptg_data, recip, measures)
+horizontal_bar_plot <- function(data, recip, measures){
+  plot_data <- hbar_plot_data(data, recip, measures)
   hbar_plot_fig(plot_data)
 }
 
 #' @importFrom dplyr %>% filter mutate group_by summarize mutate case_when left_join
-hbar_plot_data <- function(maptg_data, recip, measures){
-  maptg_data %>%
+#' @param data data frame of MAPTG format data
+#' @note after_stat transoform in hbar_plot_fig considers 0's missing data.
+#'   workaround using .001 for 0 in rate
+hbar_plot_data <- function(data, recip, measures){
+  data %>%
     filter(ascribee == recip,
            measure %in% measures)  %>%
     mutate(group=recode(group, "medicaid"="Medicaid","non_medicaid"="non-Medicaid")) %>%
@@ -18,13 +21,18 @@ hbar_plot_data <- function(maptg_data, recip, measures){
               denominator = sum(denominator),
               label = paste(numerator,denominator, sep="/")) %>%
     mutate(rate = numerator/denominator,
+           rate = case_when(
+             rate == 0 ~ 0.001,
+             is.nan(rate) ~ 0.001,
+             TRUE ~ rate),
            x_cat = paste(measure, group, sep="-"),
            x_adjust = case_when(
              group == "Medicaid" ~ 0.2,
              group == "non-Medicaid" ~ -0.2,
              TRUE ~ 0 ),
            callout = ifelse(rate < 0.81, label, NA),
-           bar_label = ifelse(is.na(callout), label, NA)) %>%
+           bar_label = ifelse(is.na(callout), label, NA)
+           ) %>%
     left_join(MR$MEASURE_NAMES, by="measure")
 }
 
@@ -33,16 +41,22 @@ hbar_plot_fig <- function(plot_data){
   breaks_y <- c(0.20, 0.4, 0.6, 0.8, 1.0)
   labels_y <- c("20%", "40%", "60%", "80%", "100%")
   x_names <- plot_data$short_name %>% unique()
+  col_width = 0.6
 
   ggplot(plot_data,
-         aes(x=stage(short_name, after_stat = (x + x_adjust)),
+         aes(x=short_name,
              y=rate, label=label, fill=group, x_adjust=x_adjust)) +
     single_bar_theme() +
-    geom_col(width = .4 ) +
-    geom_text(aes(label=bar_label), nudge_y = -0.10, hjust=0.5, color=MR$DL_FILL) +
-    geom_label(aes(label=callout), nudge_y = 0.07, hjust=0,
-               fill="white", color="black", label.r = unit(0, "lines")) +
-    scale_y_continuous(limits=c(0,1.1), expand=c(0,0),
+    geom_col(width = col_width, position=position_dodge() ) +
+    geom_text(aes(y=stage(start=rate, after_scale(y-0.1)),
+                  label=bar_label),
+               position = position_dodge(width = col_width),
+               hjust=0.5, color=MR$DL_FILL, size=2) +
+    geom_text(aes(y=stage(start=rate, after_scale(y+0.07)),
+                   label=callout),
+               position=position_dodge(width=col_width),
+               hjust=0, color="black", size=2) +
+    scale_y_continuous(limits=c(0,1.1), expand=expansion(0,add=c(0,.1)),
                        breaks=breaks_y, labels = labels_y) +
     scale_fill_manual(values = MR$REPORT_PAL, guide=guide_legend()) +
     coord_flip() +
